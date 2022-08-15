229bd3ece214063589f33ae77f788f0d7f33d814
Marcella R Pastore <marcella.rpastore@gmail.com>
2022-08-15 17:51:29 -0300

    Adicionar arquivos de projeto.

---
+++ TP_Jogo _da_Velha/Jogo da Velha.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace TP_Jogo__da_Velha
+{
+    partial class Jogo_da_Velha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn1.Location = new System.Drawing.Point(170, 50);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(141, 125);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_2.Location = new System.Drawing.Point(170, 203);
+            this.btn_2.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(141, 125);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_3.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_3.Location = new System.Drawing.Point(170, 363);
+            this.btn_3.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(141, 125);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.UseVisualStyleBackColor = false;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_4.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_4.Location = new System.Drawing.Point(348, 50);
+            this.btn_4.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(141, 125);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.UseVisualStyleBackColor = false;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_5.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_5.Location = new System.Drawing.Point(348, 203);
+            this.btn_5.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(141, 125);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.UseVisualStyleBackColor = false;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_6.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_6.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_6.Location = new System.Drawing.Point(348, 363);
+            this.btn_6.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(141, 125);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.UseVisualStyleBackColor = false;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_7.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_7.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_7.Location = new System.Drawing.Point(525, 50);
+            this.btn_7.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(141, 125);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.UseVisualStyleBackColor = false;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_8.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_8.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_8.Location = new System.Drawing.Point(525, 203);
+            this.btn_8.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(141, 125);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.UseVisualStyleBackColor = false;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btn_9.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_9.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_9.Location = new System.Drawing.Point(525, 363);
+            this.btn_9.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(141, 125);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.UseVisualStyleBackColor = false;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.BackColor = System.Drawing.Color.RosyBrown;
+            this.btn_reset.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_reset.Location = new System.Drawing.Point(348, 534);
+            this.btn_reset.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(141, 73);
+            this.btn_reset.TabIndex = 9;
+            this.btn_reset.Text = "RESET";
+            this.btn_reset.UseVisualStyleBackColor = false;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // Jogo_da_Velha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(884, 630);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Jogo_da_Velha";
+            this.Text = "Jogo_da_Velha";
+            this.Load += new System.EventHandler(this.Jogo_da_Velha_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn1;
+        private Button btn_2;
+        private Button btn_3;
+        private Button btn_4;
+        private Button btn_5;
+        private Button btn_6;
+        private Button btn_7;
+        private Button btn_8;
+        private Button btn_9;
+        private Button btn_reset;
+    }
+}
---
+++ TP_Jogo _da_Velha/Jogo da Velha.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_Jogo__da_Velha
+{
+    public partial class Jogo_da_Velha : Form
+    {
+        string[,] tabuleiro = new string[3,3];
+        string jogador1 = "X";
+        string jogador2 = "O";
+        int contador = 0;
+        string vencedor;
+
+
+        public Jogo_da_Velha()
+        {
+            InitializeComponent();
+        }
+
+        private void Jogo_da_Velha_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                if (tabuleiro[0, 0] == null)
+                {
+                    tabuleiro[0, 0] = jogador1;
+                    btn1.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            else
+            {
+                
+                if (tabuleiro[0, 0] == null)
+                {
+                    tabuleiro[0, 0] = jogador2;
+                    btn1.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+           
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                if (tabuleiro[0, 1] == null)
+                {
+                    tabuleiro[0, 1] = jogador1;
+                    btn_2.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                
+                if (tabuleiro[0, 1] == null)
+                {
+                    tabuleiro[0, 1] = jogador2;
+                    btn_2.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                if (tabuleiro[0, 2] == null)
+                {
+                    tabuleiro[0, 2] = jogador1;
+                    btn_3.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                
+                if (tabuleiro[0, 2] == null)
+                {
+                    tabuleiro[0, 2] = jogador2;
+                    btn_3.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                if (tabuleiro[1, 0] == null)
+                {
+                    tabuleiro[1, 0] = jogador1;
+                    btn_4.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                if (tabuleiro[1, 0] == null)
+                {
+                    tabuleiro[1, 0] = jogador2;
+                    btn_4.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                if (tabuleiro[1, 1] == null)
+                {
+                    tabuleiro[1, 1] = jogador1;
+                    btn_5.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                if (tabuleiro[1, 1] == null)
+                {
+                    tabuleiro[1, 1] = jogador2;
+                    btn_5.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                
+                if (tabuleiro[1, 2] == null)
+                {
+                    tabuleiro[1, 2] = jogador1;
+                    btn_6.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                
+                if (tabuleiro[1, 2] == null)
+                {
+                    tabuleiro[1, 2] = jogador2;
+                    btn_6.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                
+                if (tabuleiro[2, 0] == null)
+                {
+                    tabuleiro[2, 0] = jogador1;
+                    btn_7.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                
+                if (tabuleiro[2, 0] == null)
+                {
+                    tabuleiro[2, 0] = jogador2;
+                    btn_7.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                
+                if (tabuleiro[2, 1] == null)
+                {
+                    tabuleiro[2, 1] = jogador1;
+                    btn_8.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                
+                if (tabuleiro[2, 1] == null)
+                {
+                    tabuleiro[2, 1] = jogador2;
+                    btn_8.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            if (contador % 2 == 0)
+            {
+                
+                if (tabuleiro[2, 2] == null)
+                {
+                    tabuleiro[2, 2] = jogador1;
+                    btn_9.Text = jogador1;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+
+            }
+            else
+            {
+                
+                if (tabuleiro[2, 2] == null)
+                {
+                    tabuleiro[2, 2] = jogador2;
+                    btn_9.Text = jogador2;
+                    contador++;
+                    terminar_jogo();
+                }
+                else
+                {
+                    MessageBox.Show("Posição ocupada");
+                }
+            }
+            
+        }
+
+        private void terminar_jogo()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[i, 0] == "X" && tabuleiro[i, 1] == "X" && tabuleiro[i, 2] == "X")
+                {
+                    MessageBox.Show($"Jogador 1 - Venceu");
+                    finishGame();
+                }
+                if (tabuleiro[i, 0] == "O" && tabuleiro[i, 1] == "O" && tabuleiro[i, 2] == "O")
+                {
+                    MessageBox.Show($"Jogador 2 - Venceu");
+                    finishGame();
+                }
+
+                if (tabuleiro[0, i] == "X" && tabuleiro[1, i] == "X" && tabuleiro[2, i] == "X")
+                {
+                    MessageBox.Show($"Jogador 1 - Venceu");
+                    finishGame();
+                }
+                if (tabuleiro[0, i] == "O" && tabuleiro[1, i] == "O" && tabuleiro[2, i] == "O")
+                {
+                    MessageBox.Show($"Jogador 2 - Venceu");
+                    finishGame();
+                }
+                if (tabuleiro[0, 0] == "X" && tabuleiro[1, 1] == "X" && tabuleiro[2, 2] == "X")
+                {
+                    MessageBox.Show($"Jogador 1 - Venceu");
+                    finishGame();
+                }
+                if (tabuleiro[0, 0] == "O" && tabuleiro[1, 1] == "O" && tabuleiro[2, 2] == "O")
+                {
+                    MessageBox.Show($"Jogador 2 - Venceu");
+                    finishGame();
+                }
+                if (tabuleiro[0, 2] == "X" && tabuleiro[1, 1] == "X" && tabuleiro[2, 0] == "X")
+                {
+                    MessageBox.Show($"Jogador 1 - Venceu");
+                    finishGame();
+                }
+                if (tabuleiro[0, 2] == "O" && tabuleiro[1, 1] == "O" && tabuleiro[2, 0] == "O")
+                {
+                    MessageBox.Show($"Jogador 2 - Venceu");
+                    finishGame();
+                }
+                if (contador == 9)
+                {
+                    MessageBox.Show($"Empatou");
+                    finishGame();
+                }
+            }
+        }
+        private void finishGame()
+        {
+            Clear();
+            contador = 0;
+        }
+
+        private void Clear()
+        {
+            tabuleiro = new string[3, 3];
+            btn1.Text = String.Empty;
+            btn_2.Text = String.Empty;
+            btn_3.Text = String.Empty;
+            btn_4.Text = String.Empty;
+            btn_5.Text = String.Empty;
+            btn_6.Text = String.Empty;
+            btn_7.Text = String.Empty;
+            btn_8.Text = String.Empty;
+            btn_9.Text = String.Empty;
+        }
+
+       
+    }
+}
